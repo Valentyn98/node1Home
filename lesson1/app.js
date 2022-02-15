b1ec0dffae530ee39c933e4111c422f49666e038
Valentyn98 <valentyn.postolenko98@gmail.com>
2022-02-15 12:57:10 +0200

    Initial commit

---
+++ lesson1/app.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const fs = require('fs')
+
+
+ // fs.mkdir(path.join(__dirname, 'main','inPerson'),{recursive:true},(err) => {
+ //  if (err) {
+ //   console.log(err);
+ //  }
+ // })
+
+
+
+let onlineUsers = [
+{
+    name:'Nikola', age: 23, city:'Lviv'
+}]
+for (let itemOnline of onlineUsers) {
+
+fs.writeFile(path.join(__dirname, 'main','online', '2file.txt'),
+    `name:${itemOnline.name}\n age:${itemOnline.age}\n city:${itemOnline.city}`,(err)=>{
+        if(err){
+            console.log(err);
+        }
+})
+}
+
+ let inPersonUsers= [{
+    name:'Andrii', age: 22, city:'Lviv'
+}]
+for (let item of inPersonUsers) {
+fs.writeFile(path.join(__dirname, 'main','inPerson', '2file.txt'),
+    `name:${item.name}\n age:${item.age}\n city:${item.city}`,(err)=>{
+    if (err) {
+        console.log(err);
+    }
+})
+}
+function  swap (status){
+    fs.rename(path.join(__dirname,'inPerson','2file.txt'),path.join(__dirname,'online','file.txt'),(err)=>{
+        if(err){
+            console.log(err);
+        }
+    })
+}
+swap(true)
